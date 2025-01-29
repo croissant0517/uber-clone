01619e7e3764c8b13849e2e1ad744252c57c5565
croissant <wathyic@gmail.com>
2025-01-29 22:44:09 +0800

    feat：templete setup

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-2xl font-bold text-red-500">Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
